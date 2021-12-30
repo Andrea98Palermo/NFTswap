28bed25aaffb2210dd800dc1eac3b9a7d93e693b
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2021-12-30 17:17:30 +0100

    Initial create-ract-app skeleton

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+// import { connectWallet } from "./utils/interact.js";
+
+const Main = (props) => {
+
+  //State variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [url, setURL] = useState("");
+ 
+  useEffect(async () => { //TODO: implement
+    
+  }, []);
+
+  const connectWalletPressed = async () => { //TODO: implement
+   
+  };
+
+  const onMintPressed = async () => { //TODO: implement
+    
+  };
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <br></br>
+      <h1 id="title">🎨 NFT Swap</h1>
+    </div>
+  );
+};
+
+export default Main;
---
+++ frontend/src/util/interact.js
@@ -0,0 +1,35 @@
+export const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const addressArray = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        const obj = {
+          status: "👆🏽 Write a message in the text-field above.",
+          address: addressArray[0],
+        };
+        return obj;
+      } catch (err) {
+        return {
+          address: "",
+          status: "😥 " + err.message,
+        };
+      }
+    } else {
+      return {
+        address: "",
+        status: (
+          <span>
+            <p>
+              {" "}
+              🦊{" "}
+              <a target="_blank" href={`https://metamask.io/download.html`}>
+                You must install Metamask, a virtual Ethereum wallet, in your
+                browser.
+              </a>
+            </p>
+          </span>
+        ),
+      };
+    }
+  };
