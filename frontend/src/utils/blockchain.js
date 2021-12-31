bb8c0242f882aa9ecd907df46463074a611e507e
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2021-12-31 16:08:08 +0100

    Add simple contract call

--- frontend/src/utils/blockchain.js
+++ frontend/src/utils/blockchain.js
@@ -1,42 +1,18 @@
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import { ethers } from "ethers";
+import contract from "../contracts/contract-abi.json";
 
-require("dotenv").config();
+const contractAddress = "0x283a4E2CAa10C1a45095777F263C2BC8b252Bfda";
+const contractABI = contract.abi;
 
-const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-const web3 = createAlchemyWeb3(alchemyKey);
-
-// const contractABI = require('../contracts/contract-abi.json')
-// const contractAddress = "0xAA";
-
-/*
 export const callContract = async () => {
-
-    //load smart contract
-    window.contract = await new web3.eth.Contract(contractABI, contractAddress);//loadContract();
-
-    //set up your Ethereum transaction
-    const transactionParameters = {
-        to: contractAddress, // Required except during contract publications.
-        from: window.ethereum.selectedAddress, // must match user's active address.
-        'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress).encodeABI() //make call to NFT smart contract
-    };
-
-    //sign transaction via Metamask
-    try {
-        const txHash = await window.ethereum
-            .request({
-                method: 'eth_sendTransaction',
-                params: [transactionParameters],
-            });
-        return {
-            success: true,
-            status: "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" + txHash
-        }
-    } catch (error) {
-        return {
-            success: false,
-            status: "😥 Something went wrong: " + error.message
-        }
-    }
-}
-*/
+  try {
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const myContract = new ethers.Contract(contractAddress, contractABI, signer);
+    const message = await myContract.message();
+    return message;
+  } catch (error) {
+    return 0;
+  }
+};
