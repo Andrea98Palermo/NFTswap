02f1553ed25eef1794e621fcf9578718b4d47eea
edogab33 <edo.gab33@gmail.com>
2022-02-07 17:46:37 +0100

    Implement profile page UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   darkMode: "media",
   theme: {
-    extend: {},
-    colors: {
-      green: "#76DE65",
+    extend: {
+      colors: {
+        green: "#76DE65",
+      },
+      fontFamily: {
+        "source-code": ["\"Source Code Pro\"", "monospace"]
+      }
     },
-    fontFamily: {
-      "source-code": ["\"Source Code Pro\"", "monospace"]
-    }
   },
   plugins: [],
 }
