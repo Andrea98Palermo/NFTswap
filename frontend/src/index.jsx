d715f774f836405c2db7dcdb5f97e4981e831b33
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-11 16:56:21 +0100

    Use web3-react to handle connection status

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -10,17 +10,25 @@ import Profile from "./routes/profile"
 import "./index.css"
 import Navbar from "./components/Navbar"
 import Spacer from "./components/Spacer"
+import { Web3ReactProvider } from "@web3-react/core"
+import { Web3Provider } from "@ethersproject/providers"
+
+function getLibrary(provider) {
+  return new Web3Provider(provider)
+}
 
 const rootElement = document.getElementById("root")
 render(
   <BrowserRouter>
-    <Navbar />
-    <Spacer space={32} />
-    <Routes>
-      <Route path="/" element={<Main />}/>
-      <Route path="upload" element={<Upload />} />
-      <Route path="profile" element={<Profile />} />
-    </Routes>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Navbar />
+      <Spacer space={32} />
+      <Routes>
+        <Route path="/" element={<Main />}/>
+        <Route path="upload" element={<Upload />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+    </Web3ReactProvider>
   </BrowserRouter>,
   rootElement
 )
