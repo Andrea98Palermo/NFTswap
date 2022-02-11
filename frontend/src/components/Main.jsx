cb5286cd0a66abb95c32bcfaa443fe4332201f47
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-11 16:58:08 +0100

    Obtain NFT for the given address

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,17 +1,54 @@
 import Card from "./Card"
 import Spacer from "./Spacer"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useWeb3React } from "@web3-react/core"
+
+const client = axios.create({
+  baseURL: "https://api.opensea.io/api/v1/",
+})
 
 function Main() {
+  const [nft, setNft] = useState(null)
+  const { account, active} = useWeb3React()
+
+  useEffect(async () => {
+    const response = await client.get("assets?owner=" + account)
+    setNft(response.data)
+  }, [account])
+
+  if (!active)  {
+    return (
+      <div className="container mx-auto">
+        <h2 className="text-xl font-bold basis-full justify-center">
+        Connect a Wallet
+        </h2>
+      </div>
+    )
+  }
+
   return (
     <div className="container mx-auto">
-      <h2 className="text-xl font-bold basis-full justify-center"> Latest NFTs</h2>
-      <Spacer space={32}/>
-      <div className="container grid grid-flow-col-dense gap-2">
-        <Card title="Titolo" description="Descrizione" image="https://lh3.googleusercontent.com/3Tr3Lhlcm1Zqs8INDS6oKGe5ChtoqTouGyaPSPsSaRCM-TZZ6E3OgjUayD4xWKjeWSpNCdSOopa3E6tLIHI522M5wdXXj4Q_uN6D=w600" link="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/23443662500946835264283410846443095105284164377306294737980798252351638994945"/>
-        <Card title="Titolo" description="Descrizione" image="https://lh3.googleusercontent.com/3Tr3Lhlcm1Zqs8INDS6oKGe5ChtoqTouGyaPSPsSaRCM-TZZ6E3OgjUayD4xWKjeWSpNCdSOopa3E6tLIHI522M5wdXXj4Q_uN6D=w600" link="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/23443662500946835264283410846443095105284164377306294737980798252351638994945"/>
-        <Card title="Titolo" description="Descrizione" image="https://lh3.googleusercontent.com/3Tr3Lhlcm1Zqs8INDS6oKGe5ChtoqTouGyaPSPsSaRCM-TZZ6E3OgjUayD4xWKjeWSpNCdSOopa3E6tLIHI522M5wdXXj4Q_uN6D=w600" link="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/23443662500946835264283410846443095105284164377306294737980798252351638994945"/>
-        <Card title="Titolo" description="Descrizione" image="https://lh3.googleusercontent.com/3Tr3Lhlcm1Zqs8INDS6oKGe5ChtoqTouGyaPSPsSaRCM-TZZ6E3OgjUayD4xWKjeWSpNCdSOopa3E6tLIHI522M5wdXXj4Q_uN6D=w600" link="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/23443662500946835264283410846443095105284164377306294737980798252351638994945"/>
-        <Card title="Titolo" description="Descrizione" image="https://lh3.googleusercontent.com/3Tr3Lhlcm1Zqs8INDS6oKGe5ChtoqTouGyaPSPsSaRCM-TZZ6E3OgjUayD4xWKjeWSpNCdSOopa3E6tLIHI522M5wdXXj4Q_uN6D=w600" link="https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/23443662500946835264283410846443095105284164377306294737980798252351638994945"/>
+      <h2 className="text-xl font-bold basis-full justify-center">
+        Latest NFTs
+      </h2>
+      <Spacer space={32} />
+      <div className="container grid gap-5 grid-cols-4">
+        {!nft && <div>Loading...</div>}
+        {nft && !nft.assets.length && <div>No NFTs found</div>}
+        {nft &&
+          nft.assets.length &&
+          nft.assets.map((asset, index) => {
+            return (
+              <Card
+                key={index}
+                title={asset.name}
+                description={asset.description}
+                image={asset.image_url}
+                link={asset.permalink}
+              />
+            )
+          })}
       </div>
     </div>
   )
