51347c63a469a23a534f9f67d9f6de74863dc9c4
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-12 17:38:33 +0100

    Move NFT gallery view to `Upload` page

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,23 +1,9 @@
-import Card from "./Card"
 import Spacer from "./Spacer"
-import axios from "axios"
-import { useState, useEffect } from "react"
 import { useWeb3React } from "@web3-react/core"
 
-const client = axios.create({
-  baseURL: "https://api.opensea.io/api/v1/",
-})
 
 function Main() {
-  const [nft, setNft] = useState(null)
-  const { account, active} = useWeb3React()
-
-  useEffect(async () => {
-    if(active){
-      const response = await client.get("assets?owner=" + account)
-      setNft(response.data)
-    }
-  }, [account])
+  const { active} = useWeb3React()
 
   if (!active)  {
     return (
@@ -32,26 +18,9 @@ function Main() {
   return (
     <div className="container mx-auto">
       <h2 className="text-xl font-bold basis-full justify-center">
-        Latest NFTs
+        Home Page
       </h2>
       <Spacer space={32} />
-      <div className="container grid gap-5 grid-cols-4">
-        {!nft && <div>Loading...</div>}
-        {nft && !nft.assets.length && <div>No NFTs found</div>}
-        {nft &&
-          nft.assets.length ?
-          nft.assets.map((asset, index) => {
-            return (
-              <Card
-                key={index}
-                title={asset.name}
-                description={asset.description}
-                image={asset.image_url}
-                link={asset.permalink}
-              />
-            )
-          }) : null}
-      </div>
     </div>
   )
 }
