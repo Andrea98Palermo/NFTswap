bcd6437fb38652085ffbb1ecd0e99832ab573bba
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-14 14:03:11 +0100

    Cleanup

--- frontend/src/utils/blockchain.js
+++ frontend/src/utils/blockchain.js
@@ -6,29 +6,6 @@ const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 const contractAddress = CONTRACT_ADDRESS
 const contractABI = contract.abi
 
-export const callHelloWorld = async () => {
-  try {
-    const { ethereum } = window
-
-    if (ethereum) {
-      const provider = new ethers.providers.Web3Provider(ethereum)
-      const signer = provider.getSigner()
-      const myContract = new ethers.Contract(
-        contractAddress,
-        contractABI,
-        signer
-      )
-      let message = await myContract.helloWorld()
-      return message
-    } else {
-      console.log("Ethereum object doesn't exist!")
-      return "Error in Contract call"
-    }
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 export const callMakeProposal = async (nftAddress = "", tokenId = 0) => {
   try {
     const { ethereum } = window
