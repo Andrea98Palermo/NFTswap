84ef868026868edfa2f9ebab9fc11969107d227d
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-15 23:15:34 +0100

    Save NFT metadata on Sanity DB

--- frontend/src/routes/upload.jsx
+++ frontend/src/routes/upload.jsx
@@ -10,6 +10,7 @@ import {
   callGetApproved,
 } from "../utils/blockchain"
 import toast, { Toaster } from "react-hot-toast"
+import { sanityclient } from "../utils/sanity"
 
 const client = axios.create({
   baseURL: "https://rinkeby-api.opensea.io/api/v1/",
@@ -24,7 +25,7 @@ const initialFormData = {
 }
 
 const notify = (status, message) => {
-  switch(status){
+  switch (status) {
   case Ok:
     toast.success(message)
     break
@@ -79,7 +80,10 @@ export default function Upload() {
       setError("")
       setShowModal(true)
       setAsset(asset)
-      const approvedStatus = await callGetApproved(asset.asset_contract.address, asset.token_id)
+      const approvedStatus = await callGetApproved(
+        asset.asset_contract.address,
+        asset.token_id
+      )
       setApproved(approvedStatus)
     },
     []
@@ -87,10 +91,22 @@ export default function Upload() {
 
   const handleCardSubmit = async (event) => {
     event.preventDefault()
-    console.log(asset.asset_contract.address)
-    console.log(asset.id)
     try {
-      await callMakeProposal(asset.asset_contract.address, asset.id)
+      setLoading(true)
+      await callMakeProposal(asset.asset_contract.address, asset.token_id)
+      const nftDoc = {
+        _type: "nfts",
+        _id: `${asset.asset_contract.address}-${asset.token_id}`,
+        contractAddress: asset.asset_contract.address,
+        tokenId: asset.token_id,
+        imageUrl: asset.image_url,
+        title: asset.name,
+        description: asset.description,
+        ercType: asset.asset_contract.schema_name,
+      }
+      await sanityclient.createIfNotExists(nftDoc)
+      setLoading(false)
+      notify(Ok, "Proposal sent")
       setShowModal(false)
     } catch (error) {
       setLoading(false)
@@ -248,15 +264,35 @@ export default function Upload() {
                             type="button"
                             onClick={handleCardApprove}
                           >
-                          Approve
+                            Approve
                           </button>
                         ) : (
-                          <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed" disabled>
-                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                          <button
+                            type="button"
+                            className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed"
+                            disabled
+                          >
+                            <svg
+                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                              ></circle>
+                              <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                              ></path>
                             </svg>
-                        Processing...
+                            Processing...
                           </button>
                         )}
                       </>
@@ -268,15 +304,35 @@ export default function Upload() {
                             type="button"
                             onClick={handleCardSubmit}
                           >
-                          Propose
+                            Propose
                           </button>
                         ) : (
-                          <button type="button" className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed" disabled>
-                            <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                          <button
+                            type="button"
+                            className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed"
+                            disabled
+                          >
+                            <svg
+                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <circle
+                                className="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                strokeWidth="4"
+                              ></circle>
+                              <path
+                                className="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                              ></path>
                             </svg>
-                        Processing...
+                            Processing...
                           </button>
                         )}
                       </>
