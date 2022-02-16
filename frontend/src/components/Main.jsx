961039264a694785952782bb3ee60bd44e72555f
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-02-16 09:36:29 +0100

    Cleanup upload page

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,7 +1,10 @@
+// Our components
 import Spacer from "./Spacer"
 import Card from "./Card"
 import CardInfo from "./CardInfo"
-import { useWeb3React } from "@web3-react/core"
+import ProcessingButton from "./ProcessingButton"
+
+// Utility
 import {
   callGetAllProposals,
   callGetProposalsCount,
@@ -9,8 +12,11 @@ import {
   callApprove,
   callGetApproved
 } from "../utils/blockchain"
-import { useState, useEffect, useCallback, useReducer } from "react"
 import { sanityclient } from "../utils/sanity"
+
+// Extrernal libraries
+import { useWeb3React } from "@web3-react/core"
+import { useState, useEffect, useCallback, useReducer } from "react"
 import toast, { Toaster } from "react-hot-toast"
 
 const Ok = 0
@@ -276,33 +282,7 @@ function Main() {
                             Approve
                               </button>
                             ) : (
-                              <button
-                                type="button"
-                                className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed"
-                                disabled
-                              >
-                                <svg
-                                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                >
-                                  <circle
-                                    className="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    strokeWidth="4"
-                                  ></circle>
-                                  <path
-                                    className="opacity-75"
-                                    fill="currentColor"
-                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                  ></path>
-                                </svg>
-                            Processing...
-                              </button>
+                              <ProcessingButton />
                             )}
                           </>
                         ) : (
@@ -316,33 +296,7 @@ function Main() {
                             Bid
                               </button>
                             ) : (
-                              <button
-                                type="button"
-                                className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed"
-                                disabled
-                              >
-                                <svg
-                                  className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                >
-                                  <circle
-                                    className="opacity-25"
-                                    cx="12"
-                                    cy="12"
-                                    r="10"
-                                    stroke="currentColor"
-                                    strokeWidth="4"
-                                  ></circle>
-                                  <path
-                                    className="opacity-75"
-                                    fill="currentColor"
-                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                  ></path>
-                                </svg>
-                            Processing...
-                              </button>
+                              <ProcessingButton />
                             )}
                           </>
                         )}
--- frontend/src/routes/upload.jsx
+++ frontend/src/routes/upload.jsx
@@ -1,16 +1,22 @@
+// Our components
 import Card from "../components/Card"
 import CardInfo from "../components/CardInfo"
 import Spacer from "../components/Spacer"
-import axios from "axios"
-import { useState, useEffect, useReducer, useCallback } from "react"
-import { useWeb3React } from "@web3-react/core"
+import ProcessingButton from "../components/ProcessingButton"
+
+// Utility
+import { sanityclient } from "../utils/sanity"
 import {
   callMakeProposal,
   callApprove,
   callGetApproved,
 } from "../utils/blockchain"
+
+// Extrernal libraries
+import axios from "axios"
+import { useState, useEffect, useReducer, useCallback } from "react"
+import { useWeb3React } from "@web3-react/core"
 import toast, { Toaster } from "react-hot-toast"
-import { sanityclient } from "../utils/sanity"
 
 const client = axios.create({
   baseURL: "https://rinkeby-api.opensea.io/api/v1/",
@@ -202,16 +208,18 @@ export default function Upload() {
         {nft && !nft.assets.length && <div>No NFTs found</div>}
         {nft && nft.assets.length
           ? nft.assets.map((asset, index) => {
-            return (
-              <button key={index} onClick={handleCardClick(asset)}>
-                <Card
-                  title={asset.name}
-                  description={asset.description}
-                  image={asset.image_url}
-                  link={asset.permalink}
-                />
-              </button>
-            )
+            if(asset.name !== null && asset.description !== null) {
+              return (
+                <button key={index} onClick={handleCardClick(asset)}>
+                  <Card
+                    title={asset.name}
+                    description={asset.description}
+                    image={asset.image_url}
+                    link={asset.permalink}
+                  />
+                </button>
+              )
+            }
           })
           : null}
         <Toaster />
@@ -268,33 +276,7 @@ export default function Upload() {
                             Approve
                           </button>
                         ) : (
-                          <button
-                            type="button"
-                            className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed"
-                            disabled
-                          >
-                            <svg
-                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                            >
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                              ></circle>
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                              ></path>
-                            </svg>
-                            Processing...
-                          </button>
+                          <ProcessingButton />
                         )}
                       </>
                     ) : (
@@ -308,33 +290,7 @@ export default function Upload() {
                             Propose
                           </button>
                         ) : (
-                          <button
-                            type="button"
-                            className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-amber-500 hover:bg-amber-400 transition ease-in-out duration-150 cursor-not-allowed"
-                            disabled
-                          >
-                            <svg
-                              className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                            >
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                              ></circle>
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                              ></path>
-                            </svg>
-                            Processing...
-                          </button>
+                          <ProcessingButton />
                         )}
                       </>
                     )}
